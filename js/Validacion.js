8aadac75616ed281824a92af216f979c9d60fa70
jeroracedo <jeroraar005@gmail.com>
2020-07-15 18:31:52 -0300

    validaciones formulario

---
+++ js/Validacion.js
@@ -0,0 +1,36 @@
+function validacion_formulario(event) {
+  event.preventDefault();
+  var name = document.myform.name.value;
+  var lastname = document.myform.lastname.value;
+  var email = document.myform.email.value;
+  var phone = document.myform.phone.value;
+  var message = document.myform.message.value;
+  //name == null ||
+  console.log(document.myform);
+  if (name == "") {
+    alert("Nombre no puede estar vacio");
+
+    return false;
+  }
+  if (lastname == null || lastname == "") {
+    alert("Apellido no puede estar vacio");
+    return false;
+  }
+  if (email == null || email == "") {
+    alert("email no puede estar vacio");
+    return false;
+  }
+  if (phone == null || phone == "") {
+    alert("Celular no puede estar vacio");
+    return false;
+  }
+  if (message == null || message == "") {
+    alert("Mensaje no puede estar vacio");
+    return false;
+  }
+}
+
+function save(event) {
+  var nombre = event.target.value;
+  console.log(nombre);
+}
